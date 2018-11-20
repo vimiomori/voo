5e137e895e8d7f391c2f6019eaf8a3373beda81a
vimiomori <vivian.muchen@gmail.com>
2018-11-20 23:48:53 +0900

    update

---
+++ js/animations.js
@@ -0,0 +1,10 @@
+$(document).ready(
+    function(){
+        $(".container").css("display", "none")
+        $(".container").fadeIn(2000);
+        $("#intro").css("display", "none");
+        $(".arrow").css("display", "none");
+        $("#intro").fadeIn(1500);
+        $(".arrow").fadeIn(1500);
+    }
+);
