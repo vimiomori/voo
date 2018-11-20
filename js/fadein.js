0726adf9c96107d2ba00e05b1790b74acce895d8
vimiomori <vivian.muchen@gmail.com>
2018-11-20 23:01:54 +0900

    combine js files

--- js/fadein.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(
-    function(){
-        $("p").css("display", "none");
-        $("img").css("display", "none");
-        $("p").fadeIn(1500);
-        $("img").fadeIn(1500).slideDown(300).fadeIn(500).slideDown(300).fadeIn(500);
-
-    }
-);
